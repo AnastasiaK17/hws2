f7fb63cc3e016a0ecef7238cdf24f82a2dc6df05
AnastasiaK17 <anastasia.kokvina@icloud.com>
2023-10-01 22:10:05 +0300

    hw 08

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,20 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+               let copyState = [...state]
+            copyState.sort((a, b) => {
+                if (a.name > b.name ) return 1
+                else if (a.name < b.name) return -1
+                else return 0
+        })
+        return action.payload === 'up' ? copyState : copyState.reverse()
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(el => el.age >= 18) 
         }
         default:
             return state
