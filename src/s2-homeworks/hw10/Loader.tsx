6e03f13969d95a0b6182b7df1c92ed70d270f494
AnastasiaK17 <anastasia.kokvina@icloud.com>
2023-12-25 01:18:11 +0300

    hw10-l

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,4 @@
 import s from './Loader.module.css'
+import loader from './Spin-1s-203px.svg'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <div className={s.loader}><img src={loader}/></div>
